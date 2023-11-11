cf51e62f176a89455e9b060bbb5a2d2e8cfe1339
league <league@imac-22.attlocal.net>
2023-11-11 11:41:15 -0800

    LeagueInvaders (in progress, currently on game characters part)

--- src/LeagueInvaders.java
+++ src/LeagueInvaders.java
@@ -1,4 +1,30 @@
+import javax.swing.JFrame;
 
 public class LeagueInvaders {
+	JFrame frame;
+	public static final int WIDTH = 500;
+	public static final int HEIGHT = 800;
+	GamePanel gp;
+
+	public static void main(String[] args) {
+		LeagueInvaders leagueInvaders = new LeagueInvaders();
+		leagueInvaders.setup();
+	}
+
+
+	public LeagueInvaders() {
+		frame = new JFrame();
+		gp = new GamePanel();
+	}
+
+
+	void setup() {
+		frame.add(gp);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.setSize(WIDTH, HEIGHT);
+		frame.setVisible(true);
+		frame.addKeyListener(gp);
+
+	}
 
 }
